4df96bb430bd3bb5428f5b7c4498f9ff1594c408
amielindaas <amie.lindaas@gmail.com>
2014-11-21 11:45:06 -0600

    Week 3 assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Amie Lindaas
+## 11/21/14
+## Programming assignment 2
 
-## Write a short comment describing this function
+## The following two functions will cache the inverse of a matrix
+## to use, create a matrix
+## matrix <- matrix(rnorm(9), 3, 3)
+## then store the matrix as a 'makeCacheMatrix' object
+## ghost <- makeCacheMatrix(matrix)
+## then solve inverted matrix
+## cacheSolve(ghost)
+## if you solve a 2nd time, the function gives a cached response
 
+## This makes a cache object (ghost object) to store the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) i <<- inverse
+    getinv <- function() i
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This will check to see if there is something in the cache and 
+## if not, it will solve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    return(i)
 }
